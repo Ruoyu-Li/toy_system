57aac54218029c73c305e9ef700f1f4fb6045ea1
Ruoyu-Li <liruoyu0401@outlook.com>
2017-10-05 23:20:52 -0400

    unittest

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/toy_system'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
+TEST = True
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
